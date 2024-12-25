9fce7396c7915540d72f53034cbdb59ce58301bf
Andrey Kononenko <anaruw_13@mail.ru>
2024-12-25 20:52:41 +0300

    Code reformatted

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
